ca7aa99d86dea82e0f4eb013f342e4f3d4ed5991
Akshay Padekar <Akshay.Padekar@DuffandPhelps.com>
2020-09-13 21:37:46 +0530

    added 2d spiral array

--- algoexpert/Spiral2DArray.java
+++ algoexpert/Spiral2DArray.java
@@ -6,12 +6,46 @@ import java.util.List;
 public class Spiral2DArray {
 	
 	public static void main (String args[]) {
-		int array[][] = new int[][] {{1,2,3},{12,13,14},{11,16,15},{10,9,8}};
+		int array[][] = new int[][] {{1,2,3,4},{12,13,14,5},{11,16,15,6},{10,9,8,7}};
+		Spiral2DArray.spiralTraverse(array);
 	}
+
 	
 	public static List<Integer> spiralTraverse(int[][] array) {
 		
+		List<Integer> nums = new ArrayList<Integer>();
 		
-		return new ArrayList<Integer>();
+		if(array.length == 0 || array.length == 1)
+			return nums;
+		
+		int top = 0, bottom = array.length - 1;
+		int left = 0, right = array[0].length - 1;
+		
+		int size = array.length * array[0].length;
+		
+		while (nums.size() < size) {
+			
+			for(int i = left; i <= right && nums.size() < size; i++) {
+				nums.add(array[top][i]);
+			}
+			top++;
+			
+			for(int i = top; i <= bottom && nums.size() < size; i++) {
+				nums.add(array[i][right]);
+			}
+			right--;
+			
+			for(int i = right; i >= left && nums.size() < size; i-- ) {
+				nums.add(array[bottom][i]);
+			}
+			bottom--;
+			
+			for(int i = bottom; i >= top && nums.size() < size; i--) {
+				nums.add(array[i][left]);
+			}
+			left++;
+		}
+
+		return nums;
 	}
 }
